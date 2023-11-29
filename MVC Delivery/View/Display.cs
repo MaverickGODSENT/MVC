d4e813ac9912423d71c20182fd04ab6a1f09a300
MaverickGODSENT <galinivaov88@gmail.com>
2023-11-29 14:21:16 +0200

    gfbnf

---
+++ MVC Delivery/View/Display.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Delivery.View
+{
+    internal class Display
+    {
+        Courier courier;
+        Delivery delivery;
+
+        public Display()
+        {
+
+        }
+        public void GetValues()
+        {
+            string[] info = Console.ReadLine().Split(' ');
+            while (info[0] != "end")
+            {
+
+            }
+        }
+    }
+}
